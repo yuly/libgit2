311ed01ebeaf146dd60e1c94d7f9c31e0dbb2813
Edward Thomson <ethomson@github.com>
2017-02-24 19:27:35 +0000

    pbbbbbbbbbbbbt

--- script/appveyor-mingw.sh
+++ script/appveyor-mingw.sh
@@ -7,7 +7,7 @@ if [ "$ARCH" = "i686" ]; then
     curl -LsSO http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/4.9.2/threads-win32/sjlj/$f
   fi
   7z x $f > /dev/null
-  export PATH=`pwd`/mingw32:$PATH
+  export PATH=`pwd`/mingw32/bin:$PATH
   echo "PATH is: $PATH"
 else
   f=x86_64-4.9.2-release-win32-seh-rt_v3-rev1.7z
@@ -15,11 +15,18 @@ else
     curl -LsSO http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/4.9.2/threads-win32/seh/$f
   fi
   7z x $f > /dev/null
-  export PATH=`pwd`/mingw64:$PATH
+  export PATH=`pwd`/mingw64/bin:$PATH
   echo "PATH is: $PATH"
 fi
-ls -Flas `pwd`/mingw32
-ls -Flas `pwd`/mingw64
+if [ -d `pwd`/ming32 ]; then
+	ls -Flas `pwd`/mingw32
+	ls -Flas `pwd`/mingw32/bin
+	ls -Flas `pwd`/mingw32/i686-w64-mingw32
+fi
+if [ -d `pwd`/ming64 ]; then
+	ls -Flas `pwd`/mingw64
+	ls -Flas `pwd`/mingw64/bin
+fi
 which gcc
 gcc --version
 cmake --version
